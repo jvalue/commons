cd0ee3f5e003d339cb681a6467a2f051d84a7caa
Julian Schneider <julian.js.schneider@fau.de>
2018-08-23 13:57:22 +0200

    fixed couchdb integration tests

--- auth/src/integrationtest/java/org/jvalue/commons/auth/couchdb/BasicCredentialsRepositoryTest.java
+++ auth/src/integrationtest/java/org/jvalue/commons/auth/couchdb/BasicCredentialsRepositoryTest.java
@@ -2,7 +2,6 @@ package org.jvalue.commons.auth.couchdb;
 
 
 import org.jvalue.commons.auth.BasicCredentials;
-import org.jvalue.commons.auth.couchdb.BasicCredentialsRepository;
 import org.jvalue.commons.couchdb.RepositoryAdapter;
 import org.jvalue.commons.couchdb.test.AbstractRepositoryAdapterTest;
 import org.jvalue.commons.db.DbConnectorFactory;
@@ -11,7 +10,7 @@ public final class BasicCredentialsRepositoryTest extends AbstractRepositoryAdap
 
 	@Override
 	protected RepositoryAdapter<?, ?, BasicCredentials> doCreateAdapter(DbConnectorFactory connectorFactory) {
-		return new BasicCredentialsRepository((DbConnectorFactory) connectorFactory.createConnector(getClass().getSimpleName(), true));
+		return new BasicCredentialsRepository(connectorFactory);
 	}
 
 	@Override
--- auth/src/integrationtest/java/org/jvalue/commons/auth/couchdb/ClientRepositoryTest.java
+++ auth/src/integrationtest/java/org/jvalue/commons/auth/couchdb/ClientRepositoryTest.java
@@ -2,7 +2,6 @@ package org.jvalue.commons.auth.couchdb;
 
 import org.jvalue.commons.auth.Role;
 import org.jvalue.commons.auth.User;
-import org.jvalue.commons.auth.couchdb.UserRepository;
 import org.jvalue.commons.couchdb.RepositoryAdapter;
 import org.jvalue.commons.couchdb.test.AbstractRepositoryAdapterTest;
 import org.jvalue.commons.db.DbConnectorFactory;
@@ -11,7 +10,7 @@ public final class ClientRepositoryTest extends AbstractRepositoryAdapterTest<Us
 
 	@Override
 	protected RepositoryAdapter<?, ?, User> doCreateAdapter(DbConnectorFactory connectorFactory) {
-		return new UserRepository((DbConnectorFactory) connectorFactory.createConnector(getClass().getSimpleName(), true));
+		return new UserRepository(connectorFactory);
 	}
 
 	@Override
