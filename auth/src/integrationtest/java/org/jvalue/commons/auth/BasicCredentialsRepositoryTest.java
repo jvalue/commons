4045e6d34903a31c0633fe75ed227bddb77437ca
Andreas Bauer <dev@andreasbauer.org>
2018-08-23 15:18:13 +0200

    Use HttpServiceCheck before starting the integration tests

--- auth/src/integrationtest/java/org/jvalue/commons/auth/BasicCredentialsRepositoryTest.java
+++ auth/src/integrationtest/java/org/jvalue/commons/auth/BasicCredentialsRepositoryTest.java
@@ -1,10 +1,17 @@
 package org.jvalue.commons.auth;
 
+import org.junit.BeforeClass;
 import org.jvalue.commons.couchdb.DbConnectorFactory;
 import org.jvalue.commons.couchdb.RepositoryAdapter;
 import org.jvalue.commons.couchdb.test.AbstractRepositoryAdapterTest;
+import org.jvalue.commons.utils.HttpServiceCheck;
 
 public final class BasicCredentialsRepositoryTest extends AbstractRepositoryAdapterTest<BasicCredentials> {
+
+	@BeforeClass
+	public static void setup() {
+		HttpServiceCheck.check(HttpServiceCheck.COUCHDB_URL);
+	}
 
 	@Override
 	protected RepositoryAdapter<?, ?, BasicCredentials> doCreateAdapter(DbConnectorFactory connectorFactory) {
