4e912c6d9559d84029bfea26e0fd20d4461887ee
Andreas Bauer <andreas-bauer@users.noreply.github.com>
2019-06-18 08:04:13 +0200

    Migrate to GitLab CI and GitLab Maven Repository (#7)
    * Add Dockerfile for CI base image
    
    * Add GitLab CI file
    
    * Update DbFactory for CouchDB to get hostname from env
    
    * Refactor repository tests to respect CI env

--- couchdb-test/src/main/java/org/jvalue/commons/couchdb/test/DbFactory.java
+++ couchdb-test/src/main/java/org/jvalue/commons/couchdb/test/DbFactory.java
@@ -6,6 +6,8 @@ import org.ektorp.http.HttpClient;
 import org.ektorp.http.StdHttpClient;
 import org.ektorp.impl.StdCouchDbInstance;
 
+import java.util.Optional;
+
 /**
  * Running integration tests against CouchDB requires a admin account to be setup on that
  * CouchDB instance with "admin" "admin" credentials. This class provides easier access to
@@ -15,10 +17,13 @@ public final class DbFactory {
 
 	private DbFactory() { }
 
+	private static final String COUCHDB_HOST = Optional.ofNullable(System.getenv("ODS_IT_COUCHDB_HOST")).orElse("localhost");
+
 	public static HttpClient createHttpClient() {
 		return new StdHttpClient.Builder()
 				.username("admin")
 				.password("admin")
+				.host(COUCHDB_HOST)
 				.build();
 	}
 
